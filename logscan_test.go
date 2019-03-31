b10ae0b9e11f007635cfea5ce0041370578aa389
Gianpiero Addis <gianpiero.addis@gmail.com>
2019-03-31 18:07:45 +0200

    Use a more complex log entry

--- logscan_test.go
+++ logscan_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-var rawEntry = `127.0.0.1 - peter [9/Feb/2017:10:34:12 -0700] "GET /sample-image.png HTTP/2" 200 1479`
+var rawEntry = `127.0.0.1 - - [12/Dec/2015:18:25:11 +0100] "GET /modules/mod_bowslideshow/tmpl/images/image_shadow.png HTTP/1.1" 200 4263 "-" "Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0" "-"`
 
 func TestGetIPAddress(t *testing.T) {
 	ip := getIPAddress(rawEntry)
@@ -19,8 +19,9 @@ func TestGetResponseStatus(t *testing.T) {
 }
 
 func TestGetRequestURI(t *testing.T) {
-	URI := getRequestURI(rawEntry)
-	if URI != "/sample-image.png" {
-		t.Errorf("Expected /sample-image.png, got %s.", URI)
+	expected := "/modules/mod_bowslideshow/tmpl/images/image_shadow.png"
+	got := getRequestURI(rawEntry)
+	if got != expected {
+		t.Errorf("Expected %s, got %s.", expected, got)
 	}
 }

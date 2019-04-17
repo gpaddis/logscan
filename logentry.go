018bd9718edfc735c439024926b54f2d97c4e579
Gianpiero Addis <g.addis@lemundo.de>
2019-04-17 11:35:16 +0200

    Extract logentry to file, rename test file

---
+++ logentry.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type logEntry struct {
+	ip       string
+	status   string
+	uri      string
+	agent    string
+	datetime string
+}
+
+func createLogEntry(s string) logEntry {
+	return logEntry{
+		ip:       getIPAddress(s),
+		status:   getResponseStatus(s),
+		uri:      getRequestURI(s),
+		agent:    getUserAgent(s),
+		datetime: getDateTime(s),
+	}
+}
+
+func (l logEntry) hasPotentialThreats() bool {
+	threats := `UNION|SELECT|CHAR|CONCAT`
+	match, _ := regexp.Match(threats, []byte(l.uri))
+	return match
+}
+
+func getIPAddress(s string) string {
+	return strings.Fields(s)[0]
+}
+
+func getResponseStatus(s string) string {
+	r, _ := regexp.Compile(`\s(\d{3})\s`)
+	status := r.FindString(s)
+	return strings.TrimSpace(status)
+}
+
+func getRequestURI(s string) string {
+	r, _ := regexp.Compile(`\s/\S+.\b`)
+	URI := r.FindString(s)
+	return strings.TrimSpace(URI)
+}
+
+func getUserAgent(s string) string {
+	return strings.Split(s, "\"")[5]
+}
+
+func getDateTime(s string) string {
+	r, _ := regexp.Compile(`\[.+\]`)
+	dateTime := r.FindString(s)
+	return strings.Trim(dateTime, "[]")
+}

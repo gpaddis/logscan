c4f0000c9204bbfcf1b01dc7e23b67367bdb757c
Gianpiero Addis <g.addis@lemundo.de>
2019-04-17 15:03:47 +0200

    Update the report with a log entry, add tests

--- report.go
+++ report.go
@@ -9,13 +9,14 @@ import (
 type report map[string]attacker
 
 type attacker struct {
-	ip             string
-	requests       int
-	statusCodes    []string
-	userAgent      string
-	exampleRequest string
+	ip                string
+	userAgent         string
+	statusCodes       []string
+	exampleRequest    string
+	maliciousRequests int
 }
 
+// Check if the report already contains the IP of an attacker.
 func (r report) hasIP(ip string) bool {
 	if _, ok := r[ip]; ok {
 		return true
@@ -23,7 +24,30 @@ func (r report) hasIP(ip string) bool {
 	return false
 }
 
-// scan all entries and collect the ones containing suspicious requests.
+// Add an attacker entry to the report or update an existing one.
+func (r report) update(l logEntry) {
+	if r.hasIP(l.ip) {
+		// TODO: how to work with a pointer and update the struct field?
+		a := r[l.ip]
+		a.incrementMaliciousRequests()
+		r[l.ip] = a
+	} else {
+		r[l.ip] = attacker{
+			ip:                l.ip,
+			userAgent:         l.agent,
+			exampleRequest:    l.uri,
+			statusCodes:       []string{l.status},
+			maliciousRequests: 1,
+		}
+	}
+}
+
+// Increment the count of malicious requests from an attacker.
+func (a *attacker) incrementMaliciousRequests() {
+	a.maliciousRequests++
+}
+
+// Scan all entries and collect the ones containing suspicious requests.
 func scan(logfile string) {
 	report := make(report)
 
--- report_test.go
+++ report_test.go
@@ -15,3 +15,54 @@ func TestHasIP(t *testing.T) {
 		t.Error("Got false, expecting true.")
 	}
 }
+
+func TestInitReportEntry(t *testing.T) {
+	r := make(report)
+	l := logEntry{
+		ip:       "127.0.0.1",
+		status:   "200",
+		uri:      "/test?%20AND%20SELECT",
+		agent:    "malicious agent",
+		datetime: "11/Mar/2019:12:36:29 +0100",
+	}
+
+	r.update(l)
+	expected := attacker{
+		ip:                l.ip,
+		userAgent:         l.agent,
+		statusCodes:       []string{"200"},
+		exampleRequest:    l.uri,
+		maliciousRequests: 1,
+	}
+
+	// Test a random struct field
+	if r[l.ip].exampleRequest != expected.exampleRequest {
+		t.Error("Expecting the report to contain an attacker.")
+	}
+}
+
+func TestUpdateReportEntry(t *testing.T) {
+	r := make(report)
+	l := logEntry{
+		ip:       "127.0.0.1",
+		status:   "200",
+		uri:      "/test?%20AND%20SELECT",
+		agent:    "malicious agent",
+		datetime: "11/Mar/2019:12:36:29 +0100",
+	}
+
+	r.update(l)
+	r.update(l)
+
+	if got := r[l.ip].maliciousRequests; got != 2 {
+		t.Errorf("Expecting 2 malicious requests, got %d.", got)
+	}
+}
+
+func TestIncrementMaliciousRequests(t *testing.T) {
+	a := attacker{maliciousRequests: 1}
+	a.incrementMaliciousRequests()
+	if got := a.maliciousRequests; got != 2 {
+		t.Errorf("Expecting 2 malicious requests, got %d.", got)
+	}
+}
